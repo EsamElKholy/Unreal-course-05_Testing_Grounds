055cdae1a65abdc8a88e55c1b8cae664ed6289b1
kai <0ultrakai0@gmail.com>
2017-04-04 04:24:11 +0200

    C++ AI Behaviour Tree Tasks

---
+++ Source/S05_Testing_Grounds/ChooseNextWayPoint.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "S05_Testing_Grounds.h"
+#include "ChooseNextWayPoint.h"
+
+EBTNodeResult::Type UChooseNextWayPoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) 
+{
+	return EBTNodeResult::Type::Succeeded;
+}
